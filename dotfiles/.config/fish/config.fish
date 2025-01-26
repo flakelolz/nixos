fac04c78adc96b48785587d7350d4d05f3752599
flakelolz <michaelgonzalez.code@gmail.com>
2025-01-26 12:31:35 -0400

    update

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -10,5 +10,9 @@ end
 fish_add_path "/home/flake/.cargo/bin/"
 # fish_add_path "/home/flake/zig-0.14.0/"
 
+if not set -q TMUX
+    tmux
+end
+
 starship init fish | source
 zoxide init fish | source
